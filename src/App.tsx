9b1d8d2ea2aa3609dd28c6a64b4c854d34c2c9b2
Cristian <cristian.tirdad@gmail.com>
2021-10-21 14:30:57 +0200

    Setup basic stuff firebase and more

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useEffect } from "react";
+import "./App.css";
+import { db } from "./service/firebase";
+import { collection, query, getDocs } from "firebase/firestore";
+
+const getData = async (q: any) => {
+  const querySnapshot = await getDocs(q);
+  querySnapshot.forEach((doc) => {
+    // doc.data() is never undefined for query doc snapshots
+    console.log(doc.id, " => ", doc.data());
+  });
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  useEffect(() => {
+    const q = query(collection(db, "users"));
+    getData(q);
+  }, []);
+
+  return <div className='App'>Hello world</div>;
 }
 
 export default App;
